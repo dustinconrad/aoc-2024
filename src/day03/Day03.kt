917d58276a10dcc0258520ce222e3072b4ebf0fb
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-01 22:02:03 -0800

    prep day 3

---
+++ src/day03/Day03.kt
@@ -0,0 +1,14 @@
+package day03
+
+import readResourceAsBufferedReader
+import kotlin.math.abs
+import kotlin.math.sign
+
+fun main() {
+    println("Part 1: ${part1()}")
+}
+
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("3_1.txt").lines().toList()
+    return 1
+}
---
+++ test/day03/Day02Test 1.kt
@@ -0,0 +1,28 @@
+package day03
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03Test {
+
+//    @Test
+//    fun testPart1() {
+//        val input = """
+//
+//        """.trimIndent()
+//            .lines()
+//
+//        assertEquals(4, part1(input))
+//    }
+//
+//    @Test
+//    fun testPart2() {
+//        val input = """
+//
+//        """.trimIndent()
+//            .lines()
+//
+//        assertEquals(4, part2(input))
+//    }
+
+}
