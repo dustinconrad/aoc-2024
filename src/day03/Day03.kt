1f8da91e41f4d8966a392bfe8e95418776062e5f
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-02 21:15:35 -0800

    day 3 part 1

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,8 +1,6 @@
 package day03
 
 import readResourceAsBufferedReader
-import kotlin.math.abs
-import kotlin.math.sign
 
 fun main() {
     println("Part 1: ${part1()}")
@@ -10,5 +8,23 @@ fun main() {
 
 fun part1(): Int {
     val input = readResourceAsBufferedReader("3_1.txt").lines().toList()
-    return 1
+    return part1(input)
+}
+
+fun part1(input: List<String>): Int {
+    return input.flatMap { extractMul(it) }
+        .sumOf { it.first * it.second }
+}
+
+val MUL_REGEX = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+fun extractMul(line: String): List<Pair<Int,Int>> {
+    return MUL_REGEX.findAll(line)
+        .map {
+            val s = it.value
+            val nums = s.subSequence(4, s.length - 1)
+            val (l, r) = nums.split(",").map { n -> n.toInt() }
+            l to r
+        }
+        .toList()
 }
