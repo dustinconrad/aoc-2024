4a226ef5dcf8f3b391872b8f9452577a518f09e1
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-02 21:30:42 -0800

    Day 3 part 2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -4,6 +4,7 @@ import readResourceAsBufferedReader
 
 fun main() {
     println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
 
 fun part1(): Int {
@@ -13,6 +14,16 @@ fun part1(): Int {
 
 fun part1(input: List<String>): Int {
     return input.flatMap { extractMul(it) }
+        .sumOf { it.first * it.second }
+}
+
+fun part2(): Int {
+    val input = readResourceAsBufferedReader("3_1.txt").lines().toList()
+    return part2(input)
+}
+
+fun part2(input: List<String>): Int {
+    return extractMul2(input.joinToString("\n"))
         .sumOf { it.first * it.second }
 }
 
@@ -27,4 +38,30 @@ fun extractMul(line: String): List<Pair<Int,Int>> {
             l to r
         }
         .toList()
+}
+
+val MUL_REGEX2 = Regex("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)")
+
+fun extractMul2(line: String): List<Pair<Int,Int>> {
+    var enabled = true
+    val pairs = mutableListOf<Pair<Int,Int>>()
+    for (m in MUL_REGEX2.findAll(line)) {
+        val s = m.value
+        when {
+            s.length == 4 -> {
+                enabled = true
+            }
+            s.length == 7 && s.startsWith("d") -> {
+                enabled = false
+            }
+            else -> {
+                if (enabled) {
+                    val nums = s.subSequence(4, s.length - 1)
+                    val (l, r) = nums.split(",").map { n -> n.toInt() }
+                    pairs.add(l to r)
+                }
+            }
+        }
+    }
+    return pairs
 }
