77ba5d1698fe30c782ca7c7ae4aa55ad04a70884
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-05 21:23:11 -0800

    Day 6 part 1

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -10,12 +10,12 @@ fun main() {
 
 fun part1(): Int {
     val input = readResourceAsBufferedReader("6_1.txt").lines().toList()
-    return 1
+    return part1(input)
 }
 
 fun part1(input: List<String>): Int {
-
-    return 1
+    val coords = guardPath(input)
+    return coords.size
 }
 
 fun part2(): Int {
@@ -25,4 +25,34 @@ fun part2(): Int {
 
 fun part2(input: List<String>): Int {
     return 2
+}
+
+val dirs = mapOf(
+    (-1 to 0) to (0 to 1),
+    (0 to 1) to (1 to 0),
+    (1 to 0) to (0 to -1),
+    (0 to -1) to (-1 to 0)
+)
+
+fun guardPath(input: List<String>): Set<Pair<Int,Int>> {
+    var curr = input.mapIndexed { row, line -> row to line.indexOf("^") }
+        .first { it.second != -1 }
+
+    val result = mutableSetOf<Pair<Int,Int>>()
+    var dir = -1 to 0
+
+    while (true) {
+        result.add(curr)
+        val next = (curr.first + dir.first) to (curr.second + dir.second)
+        if ((next.first !in (0..input.lastIndex)) || (next.second !in (0..input[0].lastIndex))) {
+            break
+        }
+        if (input[next.first][next.second] == '#') {
+            dir = dirs[dir]!!
+        } else {
+            curr = next
+        }
+    }
+
+    return result
 }
