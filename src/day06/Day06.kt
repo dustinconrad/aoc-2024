75e12686155a37d03dfe85d808c405f45cba5ac5
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-05 20:48:01 -0800

    prep day 6

---
+++ src/day06/Day06.kt
@@ -0,0 +1,28 @@
+package day06
+
+import byEmptyLines
+import readResourceAsBufferedReader
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("6_1.txt").lines().toList()
+    return 1
+}
+
+fun part1(input: List<String>): Int {
+
+    return 1
+}
+
+fun part2(): Int {
+    val input = readResourceAsBufferedReader("6_1.txt").lines().toList()
+    return 2
+}
+
+fun part2(input: List<String>): Int {
+    return 2
+}
