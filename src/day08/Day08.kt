4cad840f8afc2305d823f6ff0194790b1c628930
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-07 22:43:03 -0800

    Day 8 part 2

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -8,7 +8,7 @@ import subtract
 
 fun main() {
     println("Part 1: ${part1()}")
-    //println("Part 2: ${part2()}")
+    println("Part 2: ${part2()}")
 }
 
 fun part1(): Int {
@@ -28,13 +28,20 @@ fun part1(input: List<String>): Int {
 
 }
 
-fun part2(): Long {
+fun part2(): Int {
     val input = readResourceAsBufferedReader("8_1.txt").lines().toList()
     return part2(input)
 }
 
-fun part2(input: List<String>): Long {
-    return 2
+fun part2(input: List<String>): Int{
+    val antennas = antennas(input)
+    val maxY = input.lastIndex
+    val maxX = input[0].lastIndex
+    val nodes = antennas.flatMap { antinodes2(it.value, maxY, maxX) }
+        .toSet()
+        .filter { (y, x) -> y in 0 .. maxY && x in 0 .. maxX }
+
+    return nodes.size
 }
 
 fun antennas(input: List<String>): Map<Char,List<Coord>> {
@@ -59,6 +66,32 @@ fun antinodes(nodes: Iterable<Coord>): Set<Coord> {
         val anti1 = l.addCoord(diff)
         val anti2 = r.subtract(diff)
         listOf(anti1, anti2)
+    }
+        .toSet()
+}
+
+fun antinodes2(nodes: Iterable<Coord>, maxY: Int, maxX: Int): Set<Coord> {
+    val coords = listOf(nodes, nodes)
+    val pairs = coords.getCartesianProduct()
+        .filter { (l, r) -> l != r}
+
+    return pairs.flatMap { (l, r) ->
+        val diff = l.subtract(r)
+        val result = mutableSetOf<Coord>()
+        var curr = l
+        while (curr.first in 0 .. maxY && curr.second in 0 .. maxX) {
+            result.add(curr)
+            curr = curr.addCoord(diff)
+        }
+        curr = r
+        while (curr.first in 0 .. maxY && curr.second in 0 .. maxX) {
+            result.add(curr)
+            curr = curr.subtract(diff)
+        }
+
+        val anti1 = l.addCoord(diff)
+        val anti2 = r.subtract(diff)
+        result
     }
         .toSet()
 }
