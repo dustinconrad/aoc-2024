9dca22c40a4735f8557a257b5cea0568a2697fea
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-08 22:43:42 -0800

    Day 9 part 2

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -4,7 +4,7 @@ import readResourceAsBufferedReader
 
 fun main() {
     println("Part 1: ${part1()}")
-    //println("Part 2: ${part2()}")
+    println("Part 2: ${part2()}")
 }
 
 fun part1(): Long {
@@ -20,24 +20,28 @@ fun part1(input: String): Long {
     return checksum(compacted)
 }
 
-fun part2(): Int {
-    val input = readResourceAsBufferedReader("9_1.txt").lines().toList()
+fun part2(): Long {
+    val input = readResourceAsBufferedReader("9_1.txt").readLine()
     return part2(input)
 }
 
-fun part2(input: List<String>): Int{
-    return 2
+fun part2(input: String): Long {
+    val disk = toFileParts(input)
+    val compacted = compact2(disk)
+
+    return checksum(compacted)
 }
 
 sealed class FilePart {
 
     abstract val id: Int
+    abstract val length: Int
 
-    object Empty: FilePart() {
+    data class Empty(override val length: Int): FilePart() {
         override val id = 0
     }
 
-    data class Data(override val id: Int): FilePart()
+    data class Data(override val id: Int, override val length: Int): FilePart()
 }
 
 fun toFileParts(disk: String): List<FilePart>{
@@ -46,10 +50,10 @@ fun toFileParts(disk: String): List<FilePart>{
         val digit = disk[i].digitToInt()
         if (i%2 == 0) {
             // data
-            repeat(digit) { result.add(FilePart.Data(i / 2)) }
+            repeat(digit) { result.add(FilePart.Data(i / 2, digit)) }
         } else {
             // empty
-            repeat(digit) { result.add(FilePart.Empty) }
+            repeat(digit) { result.add(FilePart.Empty(digit)) }
         }
     }
     return result
@@ -80,6 +84,59 @@ fun compact(fileparts: List<FilePart>): List<FilePart> {
     val end = result.indexOfFirst { it is FilePart.Empty }
 
     return result.subList(0, end)
+}
+
+fun compact2(fileparts: List<FilePart>): List<FilePart> {
+    val result = fileparts
+        .filter { it.length != 0 }
+        .toMutableList()
+
+    var right = result.indexOfLast { it is FilePart.Data }
+
+    fun debug() {
+        println(result.joinToString("") {
+            if (it is FilePart.Empty) {
+                "."
+            } else {
+                it.id.toString()
+            } })
+    }
+
+    while (right > 0) {
+        val data = result[right]
+        var left = 0
+        while (left < right) {
+            val candidate = result[left]
+            if (candidate is FilePart.Empty && candidate.length >= data.length) {
+                break
+            } else {
+                left += candidate.length
+            }
+        }
+        if (left < right) {
+            val empty = result[left]
+            val diff = empty.length - data.length
+            // move
+            for (i in 0 until data.length) {
+                result[left + i] = result[right - i]
+                result[right - i] = FilePart.Empty(data.length)
+            }
+            // update any leftover empties
+            if (diff > 0) {
+                for (i in 0 until diff) {
+                    result[left + data.length + i] = FilePart.Empty(diff)
+                }
+            }
+        }
+
+        // decrement right
+        do {
+            val candidate = result[right]
+            right -= candidate.length
+        } while (right > 0 && result[right] is FilePart.Empty)
+    }
+
+    return result
 }
 
 fun checksum(fileparts: List<FilePart>): Long {
