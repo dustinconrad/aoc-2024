7de1e8c177a69a14b2f159be711c30e1c18f6153
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-08 21:45:30 -0800

    Day 9 part 1

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -4,16 +4,20 @@ import readResourceAsBufferedReader
 
 fun main() {
     println("Part 1: ${part1()}")
-    println("Part 2: ${part2()}")
+    //println("Part 2: ${part2()}")
 }
 
-fun part1(): Int {
-    val input = readResourceAsBufferedReader("9_1.txt").lines().toList()
+fun part1(): Long {
+    val input = readResourceAsBufferedReader("9_1.txt").readLine()
+
     return part1(input)
 }
 
-fun part1(input: List<String>): Int {
-    return 1
+fun part1(input: String): Long {
+    val disk = toFileParts(input)
+    val compacted = compact(disk)
+
+    return checksum(compacted)
 }
 
 fun part2(): Int {
@@ -23,4 +27,62 @@ fun part2(): Int {
 
 fun part2(input: List<String>): Int{
     return 2
+}
+
+sealed class FilePart {
+
+    abstract val id: Int
+
+    object Empty: FilePart() {
+        override val id = 0
+    }
+
+    data class Data(override val id: Int): FilePart()
+}
+
+fun toFileParts(disk: String): List<FilePart>{
+    val result = mutableListOf<FilePart>()
+    for (i in disk.indices) {
+        val digit = disk[i].digitToInt()
+        if (i%2 == 0) {
+            // data
+            repeat(digit) { result.add(FilePart.Data(i / 2)) }
+        } else {
+            // empty
+            repeat(digit) { result.add(FilePart.Empty) }
+        }
+    }
+    return result
+}
+
+fun compact(fileparts: List<FilePart>): List<FilePart> {
+    val result = fileparts.toMutableList()
+
+    var left = result.indexOfFirst { it is FilePart.Empty }
+    var right = result.indexOfLast { it is FilePart.Data }
+
+    while (left < right) {
+        val tmp = result[left]
+        result[left] = result[right]
+        result[right] = tmp
+
+        // increment left
+        while(result[left] is FilePart.Data) {
+            left++
+        }
+
+        // decrement right
+        while(result[right] is FilePart.Empty) {
+            right--
+        }
+    }
+
+    val end = result.indexOfFirst { it is FilePart.Empty }
+
+    return result.subList(0, end)
+}
+
+fun checksum(fileparts: List<FilePart>): Long {
+    return fileparts.mapIndexed { idx, filePart -> idx.toLong() * filePart.id.toLong() }
+        .sum()
 }
