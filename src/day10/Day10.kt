c4ba9d46de2526d8c84205b91adae9804bb5ff61
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-09 20:59:18 -0800

    Day 10 prep

---
+++ src/day10/Day10.kt
@@ -0,0 +1,28 @@
+package day10
+
+import readResourceAsBufferedReader
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+fun part1(): Long {
+    val input = readResourceAsBufferedReader("10_1.txt").readLine()
+
+    return part1(input)
+}
+
+fun part1(input: String): Long {
+
+    return 1
+}
+
+fun part2(): Long {
+    val input = readResourceAsBufferedReader("10_1.txt").readLine()
+    return part2(input)
+}
+
+fun part2(input: String): Long {
+    return 2
+}
---
+++ test/day10/Day10Test.kt
@@ -0,0 +1,17 @@
+package day10
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day09Test {
+
+    @Test
+    fun testPart1() {
+        val input = "2333133121414131402"
+
+        assertEquals(2858, part2(input))
+    }
+
+
+
+}
