5ad8bce9710076b6967cadb70166bb10b0042844
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-09 22:03:27 -0800

    Day 10 done

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -19,13 +19,13 @@ fun part1(input: List<String>): Int {
     return scoreTrailheads(input)
 }
 
-fun part2(): Long {
+fun part2(): Int {
     val input = readResourceAsBufferedReader("10_1.txt").lines().toList()
     return part2(input)
 }
 
-fun part2(input: List<String>): Long {
-    return 2
+fun part2(input: List<String>): Int {
+    return rateTrailheads(input)
 }
 
 fun scoreTrailheads(input: List<String>): Int {
@@ -90,4 +90,66 @@ fun scoreTrailheads(input: List<String>): Int {
     }
 
     return zeroes.sumOf { (y, x) -> dp[y][x].size }
+}
+
+fun rateTrailheads(input: List<String>): Int {
+    val dp = input.map { IntArray(input[0].length) }
+
+    val q = ArrayDeque<Coord>()
+    val nines = input.flatMapIndexed { y, line ->
+        line.mapIndexed { x, c ->
+            if(c == '9') {
+                y to x
+            } else {
+                null
+            }
+        }.filterNotNull()
+    }
+    nines.forEach { curr ->
+        val (currY, currX) = curr
+        dp[currY][currX] = 1
+        // add to queue
+        listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+            .map { curr.addCoord(it) }
+            .filter { (y, x) -> y in 0 .. input.lastIndex && x in 0 .. input[0].lastIndex }
+            .filter { (y, x) -> input[y][x].isDigit() && input[y][x].digitToInt() == (input[currY][currX]).digitToInt() - 1}
+            .forEach { q.add(it) }
+    }
+
+    while (q.isNotEmpty()) {
+        val curr = q.removeFirst()
+        val (currY, currX) = curr
+        val currHeight = input[currY][currX].digitToInt()
+
+        val validNeighbors = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+            .map { curr.addCoord(it) }
+            .filter { (y, x) -> y in 0 .. input.lastIndex && x in 0 .. input[0].lastIndex }
+            .groupBy { (y, x) -> if (input[y][x].isDigit()) {
+                    input[y][x].digitToInt()
+                } else {
+                    -2
+                }
+            }
+
+        // compute current score
+        if (validNeighbors.containsKey(currHeight + 1)) {
+            dp[currY][currX] = validNeighbors[currHeight + 1]!!.sumOf { (y, x) -> dp[y][x] }
+        }
+        // q neighbors
+        validNeighbors[currHeight - 1]?.forEach {
+            q.add(it)
+        }
+    }
+
+    val zeroes = input.flatMapIndexed { y, line ->
+        line.mapIndexed { x, c ->
+            if(c == '0') {
+                y to x
+            } else {
+                null
+            }
+        }.filterNotNull()
+    }
+
+    return zeroes.sumOf { (y, x) -> dp[y][x] }
 }
