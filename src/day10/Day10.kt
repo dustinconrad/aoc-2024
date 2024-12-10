a2ef1d88e4451cbcea2abccbef560aa16ccd60de
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-09 21:59:04 -0800

    Day 10 part 1

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -1,5 +1,7 @@
 package day10
 
+import Coord
+import addCoord
 import readResourceAsBufferedReader
 
 fun main() {
@@ -7,22 +9,85 @@ fun main() {
     println("Part 2: ${part2()}")
 }
 
-fun part1(): Long {
-    val input = readResourceAsBufferedReader("10_1.txt").readLine()
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("10_1.txt").lines().toList()
 
     return part1(input)
 }
 
-fun part1(input: String): Long {
-
-    return 1
+fun part1(input: List<String>): Int {
+    return scoreTrailheads(input)
 }
 
 fun part2(): Long {
-    val input = readResourceAsBufferedReader("10_1.txt").readLine()
+    val input = readResourceAsBufferedReader("10_1.txt").lines().toList()
     return part2(input)
 }
 
-fun part2(input: String): Long {
+fun part2(input: List<String>): Long {
     return 2
+}
+
+fun scoreTrailheads(input: List<String>): Int {
+    val dp = input.map { Array<MutableSet<Coord>>(input[0].length) { mutableSetOf() } }
+
+    val q = ArrayDeque<Coord>()
+    val nines = input.flatMapIndexed { y, line ->
+        line.mapIndexed { x, c ->
+            if(c == '9') {
+                y to x
+            } else {
+                null
+            }
+        }.filterNotNull()
+    }
+    nines.forEach { curr ->
+        val (currY, currX) = curr
+        dp[currY][currX].add(curr)
+        // add to queue
+        listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+            .map { curr.addCoord(it) }
+            .filter { (y, x) -> y in 0 .. input.lastIndex && x in 0 .. input[0].lastIndex }
+            .filter { (y, x) -> input[y][x].isDigit() && input[y][x].digitToInt() == (input[currY][currX]).digitToInt() - 1}
+            .forEach { q.add(it) }
+    }
+
+    while (q.isNotEmpty()) {
+        val curr = q.removeFirst()
+        val (currY, currX) = curr
+        val currHeight = input[currY][currX].digitToInt()
+
+        val validNeighbors = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+            .map { curr.addCoord(it) }
+            .filter { (y, x) -> y in 0 .. input.lastIndex && x in 0 .. input[0].lastIndex }
+            .groupBy { (y, x) -> if (input[y][x].isDigit()) {
+                    input[y][x].digitToInt()
+                } else {
+                    -2
+                }
+            }
+
+        // compute current score
+        if (validNeighbors.containsKey(currHeight + 1)) {
+            validNeighbors[currHeight + 1]!!.forEach { (y, x) ->
+                dp[currY][currX].addAll(dp[y][x])
+            }
+        }
+        // q neighbors
+        validNeighbors[currHeight - 1]?.forEach {
+            q.add(it)
+        }
+    }
+
+    val zeroes = input.flatMapIndexed { y, line ->
+        line.mapIndexed { x, c ->
+            if(c == '0') {
+                y to x
+            } else {
+                null
+            }
+        }.filterNotNull()
+    }
+
+    return zeroes.sumOf { (y, x) -> dp[y][x].size }
 }
--- test/day10/Day10Test.kt
+++ test/day10/Day10Test.kt
@@ -3,13 +3,36 @@ package day10
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class Day09Test {
+class Da10Test {
 
     @Test
-    fun testPart1() {
-        val input = "2333133121414131402"
+    fun testPart1_1() {
+        val input = """
+        ...0...
+        ...1...
+        ...2...
+        6543456
+        7.....7
+        8.....8
+        9.....9
+        """.trimIndent().lines()
 
-        assertEquals(2858, part2(input))
+        assertEquals(2, part1(input))
+    }
+
+    @Test
+    fun testPart1_2() {
+        val input = """
+        ..90..9
+        ...1.98
+        ...2..7
+        6543456
+        765.987
+        876....
+        987....
+        """.trimIndent().lines()
+
+        assertEquals(4, part1(input))
     }
 
 
