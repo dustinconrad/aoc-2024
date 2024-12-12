6769624c825d2fa5c21d3c41c9039ff0de55e5ad
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-11 21:46:40 -0800

    Day 12 part 1

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -1,22 +1,25 @@
 package day12
 
+import Coord
+import addCoord
 import readResourceAsBufferedReader
 import java.math.BigInteger
 
 fun main() {
     println("Part 1: ${part1()}")
-    println("Part 2: ${part2()}")
+    //println("Part 2: ${part2()}")
 }
 
-fun part1(): Long {
-    val input = readResourceAsBufferedReader("12_1.txt").readLine()
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("12_1.txt").lines().toList()
 
     return part1(input)
 }
 
-fun part1(input: String, iterations: Int = 25): Long {
+fun part1(input: List<String>): Int {
+    val regions = regions(input)
 
-    return 1
+    return regions.sumOf { it.price }
 }
 
 fun part2(): Long {
@@ -26,4 +29,70 @@ fun part2(): Long {
 
 fun part2(input: String, iterations: Int = 75): Long {
     return 2
+}
+
+fun regions(input: List<String>): Set<Region> {
+    val allCoords = input.flatMapIndexed { y, line -> line.indices.map { y to it } }
+        .toMutableSet()
+
+    val result = mutableSetOf<Region>()
+    val seen = mutableSetOf<Coord>()
+
+    //println("total coords: ${allCoords.size}")
+    while (allCoords.isNotEmpty()) {
+        val start = allCoords.first()
+        val nextRegion = nextRegion(input, seen, start)
+        result.add(nextRegion)
+        //println("Regions: ${result.size}")
+        seen.addAll(nextRegion.coords)
+        allCoords.removeAll(nextRegion.coords)
+        //println("coords left: ${allCoords.size}")
+    }
+    return result
+}
+
+private val dirs = listOf(
+    (-1 to 0),
+    (0 to 1),
+    (1 to 0),
+    (0 to -1)
+)
+
+private fun nextRegion(input: List<String>, seen: Set<Coord>, start: Coord): Region {
+    val visited = mutableSetOf<Coord>()
+
+    val currChar = input[start.first][start.second]
+
+    val q = ArrayDeque<Coord>()
+    q.add(start)
+    visited.add(start)
+
+    while (q.isNotEmpty()) {
+        val head = q.removeFirst()
+        val (hy, hx) = head
+
+        dirs.map { head.addCoord(it) }
+            .filter { (y, x) -> y in 0 .. input.lastIndex && x in 0 .. input[0].lastIndex }
+            .filter { (y, x) -> input[y][x] == currChar }
+            .filter { !visited.contains(it) }
+            .forEach {
+                q.add(it)
+                visited.add(it)
+            }
+    }
+
+    return Region(visited)
+}
+
+data class Region(val coords: Set<Coord>) {
+    val area = coords.size
+
+    val perimeter by lazy {
+        coords.sumOf { p ->
+            dirs.map { d -> p.addCoord(d) }
+                .count { !coords.contains(it) }
+        }
+    }
+
+    val price = area * perimeter
 }
--- test/day12/Day12Test.kt
+++ test/day12/Day12Test.kt
@@ -6,10 +6,46 @@ import kotlin.test.assertEquals
 class Day12Test {
 
     @Test
-    fun testPart1_size() {
-        val input = "125 17"
+    fun testPart1_1() {
+        val input = """
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+        """.trimIndent().lines().toList()
 
-        assertEquals(55312, part1(input))
+        assertEquals(140, part1(input))
+    }
+
+    @Test
+    fun testPart1_2() {
+        val input = """
+        OOOOO
+        OXOXO
+        OOOOO
+        OXOXO
+        OOOOO
+        """.trimIndent().lines().toList()
+
+        assertEquals(772, part1(input))
+    }
+
+    @Test
+    fun testPart1_3() {
+        val input = """
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
+        """.trimIndent().lines().toList()
+
+        assertEquals(1930, part1(input))
     }
 
 }
