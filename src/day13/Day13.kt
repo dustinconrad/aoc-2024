5f2198c947e6b4869917ddb2aafa3c1a703b9e15
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-12 22:37:41 -0800

    longs

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -5,39 +5,41 @@ import byEmptyLines
 
 fun main() {
     println("Part 1: ${part1()}")
+    // 83341936921125 too high
     println("Part 2: ${part2()}")
 }
 
-fun part1(): Int {
+fun part1(): Long {
     val input = readResourceAsBufferedReader("13_1.txt").lines().toList().byEmptyLines()
 
     return part1(input)
 }
 
-fun part1(input: List<String>): Int {
+fun part1(input: List<String>): Long {
     return input.map { Machine.parse(it) }
         .filter { it.isPossible }
         .sumOf { it.tokens }
 }
 
-fun part2(): Int {
-    val input = readResourceAsBufferedReader("13_1.txt").lines().toList()
+fun part2(): Long {
+    val input = readResourceAsBufferedReader("13_1.txt").lines().toList().byEmptyLines()
     return part2(input)
 }
 
-fun part2(input: List<String>): Int {
-
-    return 2
+fun part2(input: List<String>): Long {
+    return input.map { Machine.parse2(it) }
+        .filter { it.isPossible }
+        .sumOf { it.tokens }
 }
 
-data class Button(val x: Int, val y: Int)
+data class Button(val x: Long, val y: Long)
 
-data class Machine(val buttonA: Button, val buttonB: Button, val x: Int, val y: Int) {
+data class Machine(val buttonA: Button, val buttonB: Button, val x: Long, val y: Long) {
 
     private val bNum = y * buttonA.x - buttonA.y * x
     private val bDenom = buttonA.x * buttonB.y - buttonA.y * buttonB.x
 
-    val isPossible = bNum % bDenom == 0
+    val isPossible = bNum % bDenom == 0L
 
     val bCount = bNum / bDenom
 
@@ -51,9 +53,9 @@ data class Machine(val buttonA: Button, val buttonB: Button, val x: Int, val y: 
             val (a, b, prize) = lines.split("\n")
             val numberPattern = Regex("\\d+")
 
-            val (ax, ay) = numberPattern.findAll(a).toList().map { it.value }.map { it.toInt() }
-            val (bx, by) = numberPattern.findAll(b).toList().map { it.value }.map { it.toInt() }
-            val (px, py) = numberPattern.findAll(prize).toList().map { it.value }.map { it.toInt() }
+            val (ax, ay) = numberPattern.findAll(a).toList().map { it.value }.map { it.toLong() }
+            val (bx, by) = numberPattern.findAll(b).toList().map { it.value }.map { it.toLong() }
+            val (px, py) = numberPattern.findAll(prize).toList().map { it.value }.map { it.toLong() }
 
             return Machine(
                 Button(ax, ay),
@@ -61,6 +63,12 @@ data class Machine(val buttonA: Button, val buttonB: Button, val x: Int, val y: 
                 px, py
             )
         }
+
+        fun parse2(lines: String): Machine {
+            val machine = parse(lines)
+            return machine.copy(x = machine.x + 10000000000000, y = machine.y + 10000000000000)
+        }
+
 
     }
 
--- test/day13/Day13Test.kt
+++ test/day13/Day13Test.kt
@@ -1,30 +1,30 @@
 package day13
 
+import byEmptyLines
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day13Test {
 
     @Test
-    fun testPart1_1() {
-        val input = """
-        AAAA
-        BBCD
-        BBCC
-        EEEC
-        """.trimIndent().lines().toList()
-
-        assertEquals(140, part1(input))
-    }
-
-    @Test
     fun testPart2_1() {
         val input = """
-        AAAA
-        BBCD
-        BBCC
-        EEEC
-        """.trimIndent().lines().toList()
+        Button A: X+94, Y+34
+        Button B: X+22, Y+67
+        Prize: X=10000000008400, Y=10000000005400
+
+        Button A: X+26, Y+66
+        Button B: X+67, Y+21
+        Prize: X=10000000012748, Y=10000000012176
+
+        Button A: X+17, Y+86
+        Button B: X+84, Y+37
+        Prize: X=10000000007870, Y=10000000006450
+
+        Button A: X+69, Y+23
+        Button B: X+27, Y+71
+        Prize: X=10000000018641, Y=10000000010279
+        """.trimIndent().lines().toList().byEmptyLines()
 
         assertEquals(80, part2(input))
     }
