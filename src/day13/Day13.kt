7765ef06fa9b0299e671aedb8f665f7091b790fc
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-12 23:05:00 -0800

    Use bigint, still not working

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -2,6 +2,7 @@ package day13
 
 import readResourceAsBufferedReader
 import byEmptyLines
+import java.math.BigInteger
 
 fun main() {
     println("Part 1: ${part1()}")
@@ -9,43 +10,50 @@ fun main() {
     println("Part 2: ${part2()}")
 }
 
-fun part1(): Long {
+fun part1(): BigInteger {
     val input = readResourceAsBufferedReader("13_1.txt").lines().toList().byEmptyLines()
 
     return part1(input)
 }
 
-fun part1(input: List<String>): Long {
+fun part1(input: List<String>): BigInteger {
     return input.map { Machine.parse(it) }
         .filter { it.isPossible }
         .sumOf { it.tokens }
 }
 
-fun part2(): Long {
+fun part2(): BigInteger {
     val input = readResourceAsBufferedReader("13_1.txt").lines().toList().byEmptyLines()
     return part2(input)
 }
 
-fun part2(input: List<String>): Long {
-    return input.map { Machine.parse2(it) }
+fun part2(input: List<String>): BigInteger {
+    val machines =  input.map { Machine.parse2(it) }
         .filter { it.isPossible }
+
+    return machines
         .sumOf { it.tokens }
 }
 
-data class Button(val x: Long, val y: Long)
+data class Button(val x: BigInteger, val y: BigInteger) {
 
-data class Machine(val buttonA: Button, val buttonB: Button, val x: Long, val y: Long) {
+    constructor(x: Long, y: Long): this(BigInteger.valueOf(x), BigInteger.valueOf(y))
+}
+
+data class Machine(val buttonA: Button, val buttonB: Button, val x: BigInteger, val y: BigInteger) {
+
+    constructor(buttonA: Button, buttonB: Button, x: Long, y: Long): this(buttonA, buttonB, BigInteger.valueOf(x), BigInteger.valueOf(y))
 
     private val bNum = y * buttonA.x - buttonA.y * x
     private val bDenom = buttonA.x * buttonB.y - buttonA.y * buttonB.x
 
-    val isPossible = bNum % bDenom == 0L
+    val isPossible = bNum % bDenom == BigInteger.ZERO
 
     val bCount = bNum / bDenom
 
     val aCount = (x - buttonB.x * bCount)/buttonA.x
 
-    val tokens = aCount * 3 + bCount
+    val tokens = aCount * BigInteger.valueOf(3) + bCount
 
     companion object {
 
@@ -53,9 +61,9 @@ data class Machine(val buttonA: Button, val buttonB: Button, val x: Long, val y:
             val (a, b, prize) = lines.split("\n")
             val numberPattern = Regex("\\d+")
 
-            val (ax, ay) = numberPattern.findAll(a).toList().map { it.value }.map { it.toLong() }
-            val (bx, by) = numberPattern.findAll(b).toList().map { it.value }.map { it.toLong() }
-            val (px, py) = numberPattern.findAll(prize).toList().map { it.value }.map { it.toLong() }
+            val (ax, ay) = numberPattern.findAll(a).toList().map { it.value }.map { BigInteger(it) }
+            val (bx, by) = numberPattern.findAll(b).toList().map { it.value }.map { BigInteger(it) }
+            val (px, py) = numberPattern.findAll(prize).toList().map { it.value }.map { BigInteger(it) }
 
             return Machine(
                 Button(ax, ay),
@@ -66,7 +74,7 @@ data class Machine(val buttonA: Button, val buttonB: Button, val x: Long, val y:
 
         fun parse2(lines: String): Machine {
             val machine = parse(lines)
-            return machine.copy(x = machine.x + 10000000000000, y = machine.y + 10000000000000)
+            return machine.copy(x = machine.x + BigInteger.valueOf(10000000000000), y = machine.y + BigInteger.valueOf(10000000000000))
         }
 
 
