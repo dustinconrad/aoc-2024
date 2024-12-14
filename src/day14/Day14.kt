09d6861c950479eca97f965b81ebaeaea88baff1
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-13 20:55:20 -0800

    {rep day 14

---
+++ src/day14/Day14.kt
@@ -0,0 +1,75 @@
+package day14
+
+import readResourceAsBufferedReader
+import byEmptyLines
+import java.math.BigInteger
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+fun part1(): Long {
+    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+
+    return part1(input)
+}
+
+fun part1(input: List<String>): Long {
+    return 1
+}
+
+fun part2(): Long {
+    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+    return part2(input)
+}
+
+fun part2(input: List<String>): Long {
+    return 2
+}
+
+data class Button(val x: BigInteger, val y: BigInteger) {
+
+    constructor(x: Long, y: Long): this(BigInteger.valueOf(x), BigInteger.valueOf(y))
+}
+
+data class Machine(val buttonA: Button, val buttonB: Button, val x: BigInteger, val y: BigInteger) {
+
+    constructor(buttonA: Button, buttonB: Button, x: Long, y: Long): this(buttonA, buttonB, BigInteger.valueOf(x), BigInteger.valueOf(y))
+
+    private val det = buttonA.x * buttonB.y - buttonA.y * buttonB.x
+
+    val isPossible = (buttonA.x * y - buttonA.y * x) % det == BigInteger.ZERO && (buttonB.y * x - buttonB.x * y) % det == BigInteger.ZERO
+
+    val bCount = (buttonA.x * y - buttonA.y * x) / det
+
+    val aCount = (buttonB.y * x - buttonB.x * y) / det
+
+    val tokens = aCount * BigInteger.valueOf(3) + bCount
+
+    companion object {
+
+        fun parse(lines: String): Machine {
+            val (a, b, prize) = lines.split("\n")
+            val numberPattern = Regex("\\d+")
+
+            val (ax, ay) = numberPattern.findAll(a).toList().map { it.value }.map { BigInteger(it) }
+            val (bx, by) = numberPattern.findAll(b).toList().map { it.value }.map { BigInteger(it) }
+            val (px, py) = numberPattern.findAll(prize).toList().map { it.value }.map { BigInteger(it) }
+
+            return Machine(
+                Button(ax, ay),
+                Button(bx, by),
+                px, py
+            )
+        }
+
+        fun parse2(lines: String): Machine {
+            val machine = parse(lines)
+            return machine.copy(x = machine.x + BigInteger.valueOf(10000000000000), y = machine.y + BigInteger.valueOf(10000000000000))
+        }
+
+
+    }
+
+}
---
+++ test/day14/Day14Test.kt
@@ -0,0 +1,25 @@
+package day14
+
+import byEmptyLines
+import java.math.BigInteger
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day14Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+        """.trimIndent().lines().toList()
+
+        assertEquals(1, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+        """.trimIndent().lines().toList()
+
+        assertEquals(2, part2(input))
+    }
+}
