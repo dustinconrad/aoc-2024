cc6227dd10b4df40f45d024e5d35b227bbbc9b5a
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-15 20:06:01 -0800

    Day 16 prep

---
+++ src/day16/Day16.kt
@@ -0,0 +1,32 @@
+package day16
+
+import Coord
+import addCoord
+import readResourceAsBufferedReader
+import kotlin.math.abs
+import kotlin.math.sign
+
+fun main() {
+    println("Part 1: ${part1()}")
+    part2(0)
+}
+
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+
+    return part1(input)
+}
+
+fun part1(input: List<String>): Int {
+    return 1
+}
+
+fun part2(): Int {
+    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+    return part2(input)
+}
+
+fun part2(input: List<String>): Int {
+
+    return 2
+}
