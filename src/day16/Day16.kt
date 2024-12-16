589948fd0c49290d7ac082aea11d32ceffdca545
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-15 20:06:56 -0800

    prep day 16

--- src/day16/Day16.kt
+++ src/day16/Day16.kt
@@ -1,18 +1,14 @@
 package day16
 
-import Coord
-import addCoord
 import readResourceAsBufferedReader
-import kotlin.math.abs
-import kotlin.math.sign
 
 fun main() {
     println("Part 1: ${part1()}")
-    part2(0)
+    println("Part 2: ${part2()}")
 }
 
 fun part1(): Int {
-    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+    val input = readResourceAsBufferedReader("16_1.txt").lines().toList()
 
     return part1(input)
 }
@@ -22,7 +18,7 @@ fun part1(input: List<String>): Int {
 }
 
 fun part2(): Int {
-    val input = readResourceAsBufferedReader("14_1.txt").lines().toList()
+    val input = readResourceAsBufferedReader("16_1.txt").lines().toList()
     return part2(input)
 }
 
