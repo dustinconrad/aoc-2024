363602b575e8f95341bf3048d900ed05e5ef0013
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-16 22:51:39 -0800

    Day 17 examples working

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -1,26 +1,35 @@
 package day17
 
-import Coord
-import addCoord
-import dirs
-import findInGrid
 import readResourceAsBufferedReader
+import java.lang.IllegalArgumentException
 
 fun main() {
-    // 146000 wrong
-    // 147628 right
+    // 7,0,0,1,7,2,2,7,7 wrong
     println("Part 1: ${part1()}")
     println("Part 2: ${part2()}")
 }
 
-fun part1(): Int {
+fun part1(): String {
     val input = readResourceAsBufferedReader("17_1.txt").lines().toList()
+
+
 
     return part1(input)
 }
 
-fun part1(input: List<String>): Int {
-    return 1
+fun part1(input: List<String>): String {
+    val digits = Regex("\\d+")
+    val a = digits.find(input[0])!!.value.toInt()
+    val b = digits.find(input[1])!!.value.toInt()
+    val c = digits.find(input[2])!!.value.toInt()
+    val (_, p) = input[4].split(":").map { it.trim() }
+    val codes = p.split(",").map { it.toInt() }
+
+    val program = Program(a, b, c, codes)
+
+    val end = execute(program)
+
+    return end.out.joinToString(",")
 }
 
 fun part2(): Int {
@@ -30,4 +39,144 @@ fun part2(): Int {
 
 fun part2(input: List<String>): Int {
     return 2
+}
+
+sealed class OpCode {
+
+    abstract fun execute(program: Program): Program
+
+    fun comboOperand(program: Program): Int {
+        val operand = program.instr[program.ip + 1]
+
+        return when(operand) {
+            4 -> program.a
+            5 -> program.b
+            6 -> program.c
+            else -> operand
+        }
+    }
+
+    fun literalOperand(program: Program): Int {
+        return program.instr[program.ip + 1]
+    }
+
+    data object Adv : OpCode() {
+        override fun execute(program: Program): Program {
+            val numerator = program.a
+            val denominator = 1.shl(comboOperand(program))
+            return program.copy(
+                a = numerator / denominator,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Bxl : OpCode() {
+        override fun execute(program: Program): Program {
+            return program.copy(
+                b = program.b xor literalOperand(program),
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Bst : OpCode() {
+        override fun execute(program: Program): Program {
+            return program.copy(
+                b = comboOperand(program) % 8,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Jnz : OpCode() {
+        override fun execute(program: Program): Program {
+            return if(program.a == 0) {
+                program.copy(ip = program.ip + 2)
+            } else {
+                program.copy(ip = literalOperand(program))
+            }
+        }
+    }
+
+    data object Bxc : OpCode() {
+        override fun execute(program: Program): Program {
+            return program.copy(
+                b = program.b xor program.c,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Out : OpCode() {
+        override fun execute(program: Program): Program {
+            return program.copy(
+                out = program.out + comboOperand(program) % 8,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Bdv : OpCode() {
+        override fun execute(program: Program): Program {
+            val numerator = program.a
+            val denominator = 2.shl(comboOperand(program))
+            return program.copy(
+                b = numerator / denominator,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    data object Cdv : OpCode() {
+        override fun execute(program: Program): Program {
+            val numerator = program.a
+            val denominator = 2.shl(comboOperand(program))
+            return program.copy(
+                c = numerator / denominator,
+                ip = program.ip + 2
+            )
+        }
+    }
+
+    companion object {
+
+        fun fromCode(code: Int): OpCode {
+            return when(code) {
+                0 -> Adv
+                1 -> Bxl
+                2 -> Bst
+                3 -> Jnz
+                4 -> Bxc
+                5 -> Out
+                6 -> Bdv
+                7 -> Cdv
+                else -> throw IllegalArgumentException("Unrecognized code: $code")
+            }
+        }
+
+    }
+
+}
+
+
+
+data class Program(val a: Int, val b: Int, val c: Int, val instr: List<Int>, val out: List<Int> = emptyList(), val ip: Int = 0) {
+
+    val isHalted = ip > instr.lastIndex
+
+    fun step(): Program {
+        val code = instr[ip]
+        val op = OpCode.fromCode(code)
+        return op.execute(this)
+    }
+
+}
+
+fun execute(program: Program): Program {
+    var currProgram = program
+    while(!currProgram.isHalted) {
+        currProgram = currProgram.step()
+    }
+    return currProgram
 }
--- test/day17/Day17Test.kt
+++ test/day17/Day17Test.kt
@@ -6,48 +6,68 @@ import kotlin.test.assertEquals
 class Day17Test {
 
     @Test
-    fun test1() {
-        val input = """
-        ###############
-        #.......#....E#
-        #.#.###.#.###.#
-        #.....#.#...#.#
-        #.###.#####.#.#
-        #.#.#.......#.#
-        #.#.#####.###.#
-        #...........#.#
-        ###.#.#####.#.#
-        #...#.....#.#.#
-        #.#.#.###.#.#.#
-        #.....#...#.#.#
-        #.###.#.#.#.#.#
-        #S..#.....#...#
-        ###############
-        """.trimIndent().lines()
+    fun test1_1() {
+        val program = Program(0,0,9, listOf(2,6))
+        val result  = program.step()
 
-        assertEquals(7036, part1(input))
+        assertEquals(result.b, 1)
     }
 
     @Test
-    fun test2_example1() {
+    fun test1_2() {
         val input = """
-        ###############
-        #.......#....E#
-        #.#.###.#.###.#
-        #.....#.#...#.#
-        #.###.#####.#.#
-        #.#.#.......#.#
-        #.#.#####.###.#
-        #...........#.#
-        ###.#.#####.#.#
-        #...#.....#.#.#
-        #.#.#.###.#.#.#
-        #.....#...#.#.#
-        #.###.#.#.#.#.#
-        #S..#.....#...#
-        ###############
+        Register A: 10
+        Register B: 0
+        Register C: 0
+
+        Program: 5,0,5,1,5,4
         """.trimIndent().lines()
 
-        assertEquals(45, part2(input))
+        assertEquals("0,1,2", part1(input))
     }
+
+    @Test
+    fun test1_3() {
+        val input = """
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+
+        Program: 0,1,5,4,3,0
+        """.trimIndent().lines()
+
+        assertEquals("4,2,5,6,7,7,7,7,3,1,0", part1(input))
+    }
+
+    @Test
+    fun test1_4() {
+        val program = Program(0,29,0, listOf(1,7))
+        val result  = program.step()
+
+        assertEquals(result.b, 26)
+    }
+
+    @Test
+    fun test1_5() {
+        val program = Program(0,2024,43690, listOf(4,0))
+        val result  = program.step()
+
+        assertEquals(result.b, 44354)
+    }
+
+    @Test
+    fun test1_example() {
+        val input = """
+        Register A: 729
+        Register B: 0
+        Register C: 0
+
+        Program: 0,1,5,4,3,0
+        """.trimIndent().lines()
+
+        assertEquals("4,6,3,5,6,3,5,2,1,0", part1(input))
+    }
+
+
+
 }
