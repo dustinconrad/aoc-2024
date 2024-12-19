b160c888db9935f249728c5017fff49b553e7bf7
Dustin Conrad <dustinaconrad@gmail.com>
2024-12-18 20:40:55 -0800

    Day 19 prep

---
+++ src/day19/Day19.kt
@@ -0,0 +1,27 @@
+package day19
+
+import readResourceAsBufferedReader
+
+fun main() {
+    // 140 wrong
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
+
+fun part1(): Int {
+    val input = readResourceAsBufferedReader("18_1.txt").lines().toList()
+    return part1(input)
+}
+
+fun part1(input: List<String>): Int {
+    return 1
+}
+
+fun part2(): Int {
+    val input = readResourceAsBufferedReader("18_1.txt").lines().toList()
+    return part2(input)
+}
+
+fun part2(input: List<String>): Int {
+    return 2
+}
